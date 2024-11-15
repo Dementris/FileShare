7a75c2418c927ba74ba1056832dd7dfa75340fa0
Dmytro Shlapak <bdlap.shl@gmail.com>
2024-11-15 18:01:20 +0200

    database update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,8 +6,7 @@ echo "Running custom management command..."
 echo $PWD
 poetry lock --no-update
 poetry install
-mkdir /app/src/fileshare/database/alembic/versions
-poetry run alembic revision --autogenerate -m "Model init"
+
 poetry run alembic upgrade head
 # Run the WSGI server
 echo "Starting ASGI server..."
