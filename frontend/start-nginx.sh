24ba8bf9a1b00ac60bc8d3bad737d31005c32600
Dmytro Shlapak <bdlap.shl@gmail.com>
2024-11-15 15:54:49 +0200

    new fix

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace $PORT in nginx configuration with the actual value from the environment
-sed -i "s/\$PORT/${PORT}/g" /etc/nginx/conf.d
+sed -i "s/\$PORT/${PORT}/g" /etc/nginx/conf.d/nginx.conf
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
